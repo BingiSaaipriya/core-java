60dfe3bf65410f290641cc8fb69915e26c3bbca0
shyam <shyam@SHYAM>
2023-05-05 10:35:37 +0530

    java

---
+++ java/src/com/basic/Employeedetails.java
@@ -0,0 +1,25 @@
+package com.basic;
+
+public class Employeedetails {
+	public static void main(String[] args) {
+		
+	
+	int id=1;
+	String name="shyam";
+	double salary=2300.00;
+	char gender='m';
+	String address="ramagundam";
+	long phonenum=8765499897l;
+	
+	System.out.println("employee id:"+id);
+	System.out.println("employee name :"+name);
+	System.out.println("employee salary:"+salary);
+	System.out.println("employee gender:"+gender);
+	System.out.println("employee address:"+address);
+	System.out.println("employee phonenum:"+phonenum);
+
+	
+	
+	
+}
+}
