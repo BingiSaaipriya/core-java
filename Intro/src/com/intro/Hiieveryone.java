a15d82f3340594f25ccc30dec802e6eccc994753
shyam <shyam@SHYAM>
2023-05-31 12:45:30 +0530

    Collections

---
+++ Intro/src/com/intro/Hiieveryone.java
@@ -0,0 +1,14 @@
+package com.intro;
+
+public class Hiieveryone {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		int a= 10;
+				int b=20;
+		int  sum=a+b;
+		System.out.println(sum);
+		
+	}
+
+}
